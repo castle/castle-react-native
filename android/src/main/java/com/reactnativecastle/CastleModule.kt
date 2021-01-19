6b73752fca7d3513d460ee8c954a3904b9919466
Sebastian Simson <sebastian@simsons.se>
2021-01-19 13:43:14 +0100

    Add basic implementation of Android support

--- android/src/main/java/com/reactnativecastle/CastleModule.kt
+++ android/src/main/java/com/reactnativecastle/CastleModule.kt
@@ -1,9 +1,9 @@
 package com.reactnativecastle
 
-import com.facebook.react.bridge.ReactApplicationContext
-import com.facebook.react.bridge.ReactContextBaseJavaModule
-import com.facebook.react.bridge.ReactMethod
-import com.facebook.react.bridge.Promise
+import android.app.Application
+import com.facebook.react.bridge.*
+import io.castle.android.Castle
+import io.castle.android.CastleConfiguration
 
 class CastleModule(reactContext: ReactApplicationContext) : ReactContextBaseJavaModule(reactContext) {
 
@@ -11,14 +11,120 @@ class CastleModule(reactContext: ReactApplicationContext) : ReactContextBaseJava
         return "Castle"
     }
 
-    // Example method
-    // See https://reactnative.dev/docs/native-modules-android
     @ReactMethod
-    fun multiply(a: Int, b: Int, promise: Promise) {
-    
-      promise.resolve(a * b)
-    
+    fun clientIdHeaderName(promise: Promise) {
+      promise.resolve(Castle.clientIdHeaderName)
     }
 
-    
+    @ReactMethod
+    fun versionString(promise: Promise) {
+      promise.resolve(BuildConfig.VERSION_NAME)
+    }
+
+    @ReactMethod
+    fun configure(options: ReadableMap?, promise: Promise) {
+      if (options != null) {
+        val builder = CastleConfiguration.Builder()
+        builder.publishableKey(options.getString("publishableKey"))
+          .screenTrackingEnabled(options.getBoolean("screenTrackingEnabled"))
+          .debugLoggingEnabled(options.getBoolean("debugLoggingEnabled"))
+          .maxQueueLimit(options.getInt("maxQueueLimit"))
+          .flushLimit(options.getInt("flushLimit"))
+          .useCloudflareApp(options.getBoolean("useCloudflareApp"))
+          .apiDomain(options.getString("apiDomain"))
+          .apiPath(options.getString("apiPath"))
+
+        Castle.configure(reactApplicationContext.applicationContext as Application, builder.build())
+
+        promise.resolve(null)
+      } else {
+        promise.reject("Invalid configuration")
+      }
+    }
+
+    @ReactMethod
+    fun configureWithPublishableKey(publishableKey: String) {
+      Castle.configure(reactApplicationContext.applicationContext as Application, publishableKey)
+    }
+
+    @ReactMethod
+    fun resetConfiguration() {
+      Castle.reset()
+    }
+
+    @ReactMethod
+    fun identify(identifier: String, traits: ReadableMap) {
+      var traitsMap = mutableMapOf<String, String>()
+      val itr = traits.keySetIterator()
+      while (itr.hasNextKey()) {
+        val key = itr.nextKey()
+        if (traits.getType(key) == ReadableType.String) {
+          val value = traits.getString(key)
+          value?.let { traitsMap.put(key, it) }
+        }
+      }
+
+      Castle.identify(identifier, traitsMap)
+    }
+
+    @ReactMethod
+    fun secure(signature: String) {
+      Castle.secure(signature)
+    }
+
+    @ReactMethod
+    fun screen(screenName: String) {
+      Castle.screen(screenName)
+    }
+
+    @ReactMethod
+    fun flush() {
+      Castle.flush()
+    }
+
+    @ReactMethod
+    fun flushIfNeeded(url: String) {
+      Castle.flushIfNeeded(url)
+    }
+
+    @ReactMethod
+    fun reset() {
+      Castle.reset()
+    }
+
+    @ReactMethod
+    fun isWhitelistUrl(url: String, promise: Promise) {
+      promise.resolve(true)
+
+    }
+
+    @ReactMethod
+    fun baseUrl(promise: Promise) {
+      promise.resolve(Castle.baseUrl())
+    }
+
+    @ReactMethod
+    fun clientId(promise: Promise) {
+      promise.resolve(Castle.clientId())
+    }
+
+    @ReactMethod
+    fun userId(promise: Promise) {
+      promise.resolve(Castle.userId())
+    }
+
+    @ReactMethod
+    fun userSignature(promise: Promise) {
+      promise.resolve(Castle.userSignature())
+    }
+
+    @ReactMethod
+    fun userAgent(promise: Promise) {
+      promise.resolve(Castle.userAgent())
+    }
+
+    @ReactMethod
+    fun queueSize(promise: Promise) {
+      promise.resolve(Castle.queueSize())
+    }
 }
