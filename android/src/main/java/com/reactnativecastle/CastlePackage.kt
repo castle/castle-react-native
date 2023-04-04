b5e4dd8086f376da48238c25b476ed1aa70f11e1
Alexander Simson <alex@simsons.se>
2023-04-04 15:25:53 +0200

    Upgrade example application to use React Native 0.69 (#79)
    * chore(dependencies): upgrade example application to use React Native 0.69
    
    * chore(cleanup): remove unnecessary files, update android package names
    
    * chore(cleanup): move files, revert some implementation changes
    
    * chore(cleanup): move ReactNativeFlipper.java
    
    * chore(example): fix android build
    
    * chore(ci): update circle ci image
    
    * chore(ci): update config
    
    * chore(dependencies): add metro-react-native-babel-preset dev dependency
    
    * chore(ci): update nodejs image version

--- android/src/main/java/com/reactnativecastle/CastlePackage.kt
+++ android/src/main/java/com/reactnativecastle/CastlePackage.kt
@@ -1,17 +1,18 @@
 package com.reactnativecastle
 
+import java.util.Arrays
+
 import com.facebook.react.ReactPackage
 import com.facebook.react.bridge.NativeModule
 import com.facebook.react.bridge.ReactApplicationContext
 import com.facebook.react.uimanager.ViewManager
 
-
 class CastlePackage : ReactPackage {
     override fun createNativeModules(reactContext: ReactApplicationContext): List<NativeModule> {
-        return listOf(CastleModule(reactContext))
+        return Arrays.asList<NativeModule>(CastleModule(reactContext))
     }
 
     override fun createViewManagers(reactContext: ReactApplicationContext): List<ViewManager<*, *>> {
-        return emptyList()
+        return emptyList<ViewManager<*, *>>()
     }
 }
