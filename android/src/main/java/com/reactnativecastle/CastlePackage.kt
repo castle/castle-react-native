747e7345ae0f391bedf14310a091a25f7e063904
Alexander Simson <alex@simsons.se>
2023-04-04 11:33:12 +0200

    chore(cleanup): move files, revert some implementation changes

--- android/src/main/java/com/reactnativecastle/CastlePackage.kt
+++ android/src/main/java/com/reactnativecastle/CastlePackage.kt
@@ -1,17 +1,20 @@
 package com.reactnativecastle
 
+import java.util.Arrays
+import java.util.Collections
+
 import com.facebook.react.ReactPackage
 import com.facebook.react.bridge.NativeModule
 import com.facebook.react.bridge.ReactApplicationContext
 import com.facebook.react.uimanager.ViewManager
-
+import com.facebook.react.bridge.JavaScriptModule
 
 class CastlePackage : ReactPackage {
     override fun createNativeModules(reactContext: ReactApplicationContext): List<NativeModule> {
-        return listOf(CastleModule(reactContext))
+        return Arrays.asList<NativeModule>(CastleModule(reactContext))
     }
 
     override fun createViewManagers(reactContext: ReactApplicationContext): List<ViewManager<*, *>> {
-        return emptyList()
+        return emptyList<ViewManager<*, *>>()
     }
 }
