424a1c4aa820109d7cdd9f350313af383733947f
Alexander Simson <alex@simsons.se>
2025-01-15 12:14:12 +0100

    chore(deps): update base project and example to use React Native 0.75.x

---
+++ package/lib/commonjs/index.js
@@ -0,0 +1,17 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.default = exports.Castle = void 0;
+var _reactNative = require("react-native");
+const {
+  Castle
+} = _reactNative.NativeModules;
+exports.Castle = Castle;
+if (!Castle) {
+  throw new Error('Failed to load Castle native module.');
+}
+var _default = Castle;
+exports.default = _default;
+//# sourceMappingURL=index.js.map
---
+++ package/lib/module/index.js
@@ -0,0 +1,9 @@
+import { NativeModules } from 'react-native';
+export const {
+  Castle
+} = NativeModules;
+if (!Castle) {
+  throw new Error('Failed to load Castle native module.');
+}
+export default Castle;
+//# sourceMappingURL=index.js.map
