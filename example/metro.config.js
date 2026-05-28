9b2f7082249b8e4ef546b98f8fa85cb67bd444fa
Alexander Simson <alex@simsons.se>
2026-05-28 09:56:10 +0200

    Security audit fixes and RN example bump (#168)
    * chore(deps): zero npm audit vulnerabilities; drop deprecated codecov
    
    * chore(example): upgrade RN to 0.85.3 for Xcode 26.5 support
    
    * chore(deps): update java version
    
    ---------
    
    Co-authored-by: Sebastian Simson <sebastian@simsons.se>

--- example/metro.config.js
+++ example/metro.config.js
@@ -1,7 +1,6 @@
 const { getDefaultConfig, mergeConfig } = require('@react-native/metro-config');
 
 const path = require('path');
-const exclusionList = require('metro-config/src/defaults/exclusionList');
 const escape = require('escape-string-regexp');
 const pak = require('../package.json');
 
@@ -22,13 +21,10 @@ const config = {
   watchFolders: [root],
 
   // We need to make sure that only one version is loaded for peerDependencies
-  // So we blacklist them at the root, and alias them to the versions in example's node_modules
+  // So we blocklist them at the root, and alias them to the versions in example's node_modules
   resolver: {
-    exclusionListRE: exclusionList(
-      modules.map(
-        (m) =>
-          new RegExp(`^${escape(path.join(root, 'node_modules', m))}\\/.*$`)
-      )
+    blockList: modules.map(
+      (m) => new RegExp(`^${escape(path.join(root, 'node_modules', m))}\\/.*$`)
     ),
 
     extraNodeModules: modules.reduce((acc, name) => {
